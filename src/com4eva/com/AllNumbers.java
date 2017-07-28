1a18e56d2c180e397fd713adc97a9fab7ef91c2e
harambe143 <lava31@mail.com>
2017-07-28 15:03:02 -0400

    These are my changes

---
+++ src/com4eva/com/AllNumbers.java
@@ -0,0 +1,37 @@
+package com4eva.com;
+
+public class AllNumbers {
+	
+	private int x;
+
+	public static void main(String[] args) {
+		/**
+		 *  The following methods identifies even numbers
+		 *  @author 
+		 *  
+		 */
+		
+		for(int x = 1;x <101; x++ ) 
+		{
+			if(x%2 == 0)
+			{
+		
+				System.out.println(x +"is an even number");
+			}
+		
+			else {
+			
+			   System.out.println(x);
+			}
+				
+		}
+		 
+		
+		
+
+	
+		// TODO Auto-generated method stub
+
+	}
+
+}
